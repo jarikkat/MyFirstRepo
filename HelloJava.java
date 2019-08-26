394e5e28c046acd9dc405929db9e9e09ef8bc284
mrsarkar <mrinmay_sarkar@mentor.com>
2019-08-26 13:59:17 +0530

    Update HelloJava.java
    Updated HelloJava.java

--- HelloJava.java
+++ HelloJava.java
@@ -3,6 +3,8 @@ public class HelloJava {
 	public static void main(String[] args) {
 
 		System.out.println("Hello Java");
+		System.out.println("Hello Java again");
+		System.out.println("Hello Java yet again");
 		  
 		  
 	
